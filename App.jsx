0cf775fc96e914133cb8fff8b0a6c7340bbf2cab
Ick Hyun Kwon <kwonih1020@gmail.com>
2021-07-15 16:53:25 -0400

    practice state and setStatw with plus minus function

--- App.jsx
+++ App.jsx
@@ -2,33 +2,48 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Alert, ScrollView } from 'react-native';
 import PopupButton from './components/PopupButton';
 import data from './data.json';
-const tempList = [
-  { title: 1 },
-  { title: 2 },
-  { title: 3 },
-  { title: 4 },
-  { title: 5 },
-];
+import PlusButton from './components/PlusButton';
+import MinusButton from './components/MinusButton';
+
+// const tempList = [
+//   { title: 1 },
+//   { title: 2 },
+//   { title: 3 },
+//   { title: 4 },
+//   { title: 5 },
+// ];
 
 export default function App() {
-  const [state, setState] = useState(tempList);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setState(data.diary);
-    }, 5000);
-  }, []);
+  const [state, setState] = useState(0);
+  const Minus = () => {
+    setState(state - 1);
+  };
+  const Plus = () => {
+    setState(state + 1);
+  };
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setState(data.diary);
+  //   }, 5000);
+  // }, []);
 
   return (
-    <ScrollView contentContainerStyle={styles.contianer}>
-      {state.map((content, i) => {
-        return (
-          <View key={i}>
-            <Text>{content.title}</Text>
-          </View>
-        );
-      })}
-    </ScrollView>
+    // <ScrollView contentContainerStyle={styles.contianer}>
+    //   {state.map((content, i) => {
+    //     return (
+    //       <View key={i}>
+    //         <Text>{content.title}</Text>
+    //       </View>
+    //     );
+    //   })}
+    // </ScrollView>
+    <View style={styles.contianer}>
+      <Text style={styles.count}>카운터 : {state}</Text>
+      <View style={styles.buttonContainer}>
+        <PlusButton Plus={Plus} />
+        <MinusButton Minus={Minus} />
+      </View>
+    </View>
   );
 }
 
@@ -37,5 +52,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  count: {
+    fontSize: 20,
+  },
+  buttonContainer: {
+    marginTop: 50,
+    flexDirection: 'row',
   },
 });
